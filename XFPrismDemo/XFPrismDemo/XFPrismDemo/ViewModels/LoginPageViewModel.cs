594873c7ca08d44c570c77fda810cc1b336321ce
panos infolyseis <pskyvalidas@yahoo.com>
2018-01-30 14:15:11 +0200

    feat: Configure LoginPageViewModel

--- XFPrismDemo/XFPrismDemo/XFPrismDemo/ViewModels/LoginPageViewModel.cs
+++ XFPrismDemo/XFPrismDemo/XFPrismDemo/ViewModels/LoginPageViewModel.cs
@@ -1,16 +1,49 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Prism.Navigation;
 
 namespace XFPrismDemo.ViewModels
 {
 	public class LoginPageViewModel : BindableBase
 	{
-        public LoginPageViewModel()
-        {
+        private readonly INavigationService _navigationService;
+        private string _username;
+        private string _password;
+        private bool _isPasswordValid;
 
+        public LoginPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
         }
-	}
+
+        public string Username
+        {
+            get { return _username; }
+            set { SetProperty(ref _username, value); }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { SetProperty(ref _password, value); }
+        }
+
+        public bool CanLogin
+        {
+            get { return !string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password) && IsPasswordValid; }
+        }
+
+        public bool IsPasswordValid
+        {
+            get {return _isPasswordValid; }
+            set { SetProperty(ref _isPasswordValid, value); }
+        }
+
+        public DelegateCommand LoginCommand => new DelegateCommand(Login).ObservesCanExecute(() => CanLogin);        
+
+        private async void Login()
+        {
+            await _navigationService.NavigateAsync("NavigationMenuPage/NavigationPage/MainPage");
+        }
+    }
 }
