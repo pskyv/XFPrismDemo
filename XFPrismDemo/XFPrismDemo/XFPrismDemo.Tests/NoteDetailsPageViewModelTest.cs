17eedc6532686b27cbea6358ff80723327b43e27
panos infolyseis <pskyvalidas@yahoo.com>
2018-04-26 17:20:02 +0300

    feat: Add Test project

---
+++ XFPrismDemo/XFPrismDemo/XFPrismDemo.Tests/NoteDetailsPageViewModelTest.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using NUnit.Framework;
+using Prism.Navigation;
+using XFPrismDemo.Services;
+using XFPrismDemo.ViewModels;
+
+namespace XFPrismDemo.Tests
+{
+    [TestFixture]
+    public class NoteDetailsPageViewModelTest
+    {
+        NoteDetailsPageViewModel _vm;
+
+        [SetUp]
+        public void Setup()
+        {
+            var mockDbSrv = new Mock<IDatabaseService>();
+            var mockNavSrv = new Mock<INavigationService>();
+
+            _vm = new NoteDetailsPageViewModel(mockNavSrv.Object, mockDbSrv.Object);
+        }
+
+        [Test]
+        public void CurrentNote_IsSet_WithNavParam_Test()
+        {
+            var mockNavParams = new Mock<NavigationParameters>().Object;
+            _vm.CurrentNote = null;
+
+            _vm.OnNavigatingTo(mockNavParams);
+
+            Assert.IsNotNull(_vm.CurrentNote, "CurrentNote is null after navigating to NoteDetailsPage");
+        }
+    }
+}
---
+++ XFPrismDemo/XFPrismDemo/XFPrismDemo.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("XFPrismDemo.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XFPrismDemo.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2da727e9-b2f0-437e-aa97-c692946adfc2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
